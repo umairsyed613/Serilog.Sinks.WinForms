a6c6d72dc141eb6c25fca648a0951edfa902e81a
Umair Hussein <umahus@melinmedical.com>
2020-08-20 15:57:56 +0200

    form changes

--- Sample/TestApplication/Form1.Designer.cs
+++ Sample/TestApplication/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.simpleLogTextBox1 = new Serilog.WinForms.SimpleLogTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // logTextBox1
@@ -39,18 +41,18 @@
             this.logTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.logTextBox1.Location = new System.Drawing.Point(7, 239);
+            this.logTextBox1.Location = new System.Drawing.Point(7, 273);
             this.logTextBox1.LogBorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.logTextBox1.LogPadding = new System.Windows.Forms.Padding(3);
             this.logTextBox1.Name = "logTextBox1";
             this.logTextBox1.ReadOnly = true;
             this.logTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.logTextBox1.Size = new System.Drawing.Size(546, 158);
+            this.logTextBox1.Size = new System.Drawing.Size(546, 187);
             this.logTextBox1.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(449, 23);
+            this.button1.Location = new System.Drawing.Point(474, 11);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -60,9 +62,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(23, 26);
+            this.textBox1.Location = new System.Drawing.Point(15, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(401, 20);
+            this.textBox1.Size = new System.Drawing.Size(453, 20);
             this.textBox1.TabIndex = 1;
             // 
             // simpleLogTextBox1
@@ -78,17 +80,39 @@
             this.simpleLogTextBox1.Size = new System.Drawing.Size(546, 158);
             this.simpleLogTextBox1.TabIndex = 4;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Json Formatted Log";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Simple Formatted Log";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(561, 410);
+            this.ClientSize = new System.Drawing.Size(561, 473);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.simpleLogTextBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.logTextBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Simple and Json Text Logging Example";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +124,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private Serilog.WinForms.SimpleLogTextBox simpleLogTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
